7a7e30c8ef2666261560b7873f0ad4d6d670ef26
Vince Liu <faceach@gmail.com>
2012-02-29 21:37:19 +0800

    html 5 tags

---
+++ web/_scripts/html5tags.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    document.createElement("header");
+    document.createElement("nav");
+    document.createElement("aside");
+    document.createElement("section");
+    document.createElement("article");
+    document.createElement("footer");
+})();
